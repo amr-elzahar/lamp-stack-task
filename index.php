c7c5eefe8f14a829b8860244fbf15e06fd6bf6b7
Amr Elzahar <amr92621@gmail.com>
2023-07-09 11:00:00 +0300

    root commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+//Connection variables
+$servername = "localhost";
+$username = "atw";
+$password = "111";
+$dbname = "atw-task";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get visitor IP address
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+// Get current time
+$current_time = date("Y-m-d H:i:s");
+
+// Check if IP address already exists in database
+$sql = "SELECT * FROM visitors WHERE ip_address = '$ip_address'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // IP address already exists in database, fetch it and echo it
+  $row = $result->fetch_assoc();
+  echo "Hello again! Your IP address is " . $row["ip_address"] . " and the current time is " . $current_time;
+} else {
+  // IP address does not exist in database, store it and echo it
+  $sql = "INSERT INTO visitors (ip_address, visit_time) VALUES ('$ip_address', '$current_time')";
+  $conn->query($sql);
+  echo "Hello! Your IP address is " . $ip_address . " and the current time is " . $current_time;
+}
+
+// Close connection
+$conn->close();
+?>
